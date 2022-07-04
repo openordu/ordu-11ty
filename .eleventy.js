b1ef958159f06ea633b8db4aff96f665c7652ccf
Chris Godwin <patchshorts@gmail.com>
2022-07-04 12:56:06 -0500

    update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,61 +1,124 @@
-
-const eleventyPluginSyntaxHighlighter = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { DateTime }                    = require("luxon");
+const navigationPlugin                = require('@11ty/eleventy-navigation')
 const pluginMermaid                   = require("@kevingimbel/eleventy-plugin-mermaid");
+const rssPlugin                       = require('@11ty/eleventy-plugin-rss')
 const markdownIt                      = require("markdown-it");
-const markdownItEmoji                 = require("markdown-it-emoji");
-const markdownItFootnote              = require("markdown-it-footnote");
-const markdownItContainer             = require("markdown-it-container");
-const markdownItTips                  = require("markdown-it-tips-bulma");
-const markdownItAlign                 = require("markdown-it-align");
-const markdownItKatex                 = require("markdown-it-texmath");
 const markdownItSub                   = require("markdown-it-sub");
 const markdownItSup                   = require("markdown-it-sup");
-const markdownItTableOfContents       = require("markdown-it-table-of-contents");
-const markdownItTaskLists             = require("markdown-it-task-lists");
+const markdownItTips                  = require("markdown-it-tips");
+const markdownItAttrs                 = require("markdown-it-attrs");
 const markdownItVideo                 = require("markdown-it-video");
+const markdownItEmoji                 = require("markdown-it-emoji");
+const markdownItAlign                 = require("markdown-it-align");
+const markdownItKatex                 = require("markdown-it-texmath");
 const markdownItAnchor                = require("markdown-it-anchor");
-const markdownItAttrs                 = require("markdown-it-attrs")
+const markdownItFootnote              = require("markdown-it-footnote");
+const markdownItContainer             = require("markdown-it-container");
+const markdownItTaskLists             = require("markdown-it-task-lists");
+const eleventyNavigationPlugin        = require("@11ty/eleventy-navigation");
+const markdownItTableOfContents       = require("markdown-it-table-of-contents");
+const eleventyPluginSyntaxHighlighter = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
-    // assets we want to passthrough
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  
+  // assets we want to passthrough
     
-    eleventyConfig.addPassthroughCopy('./src/main.css');
-    eleventyConfig.addPassthroughCopy('./src/lib/main.js');
-    eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/main.css');
+  eleventyConfig.addPassthroughCopy('./src/lib/main.js');
+  eleventyConfig.addPassthroughCopy('./src/assets');
 
-    // for markdown extensions
-    let options = {
-      html: true
-    };
-    let markdownLibrary = markdownIt(options).use(markdownItEmoji);
-    
-    markdownLibrary.use(markdownItFootnote);
-    markdownLibrary.use(markdownItKatex);
-    markdownLibrary.use(markdownItTips);
-    markdownLibrary.use(markdownItAlign);
-    markdownLibrary.use(markdownItSub);
-    markdownLibrary.use(markdownItSup);
-    markdownLibrary.use(markdownItTableOfContents);
-    markdownLibrary.use(markdownItTaskLists);
-    markdownLibrary.use(markdownItVideo);
-    markdownLibrary.use(markdownItAnchor);
+  // for markdown extensions
+  let options = {
+    html: true
+  };
+  let markdownLibrary = markdownIt(options).use(markdownItEmoji);
+  
+  markdownLibrary.use(markdownItFootnote);
+  markdownLibrary.use(markdownItKatex);
+  markdownLibrary.use(markdownItTips);
+  markdownLibrary.use(markdownItAlign);
+  markdownLibrary.use(markdownItSub);
+  markdownLibrary.use(markdownItSup);
+  markdownLibrary.use(markdownItTableOfContents);
+  markdownLibrary.use(markdownItTaskLists);
+  markdownLibrary.use(markdownItVideo);
+  markdownLibrary.use(markdownItAnchor, {
+    permalink: markdownItAnchor.permalink.linkInsideHeader({
+      symbol: `<span class="visually-hidden">Jump to heading</span>
+      <span aria-hidden="true">#</span>`,
+      placement: 'after'
+    })
+  });
+  eleventyConfig.setLibrary("md", markdownLibrary);
 
-    eleventyConfig.setLibrary("md", markdownLibrary);
+  // for mermaid
+  eleventyConfig.addPlugin(pluginMermaid, {
+    // load mermaid from local assets directory
+    mermaid_js_src: '/assets/mermaid.min.js',
+    html_tag: 'div',
+    extra_classes: 'graph'
+  });
 
-    // for mermaid
-    eleventyConfig.addPlugin(pluginMermaid, {
-      // load mermaid from local assets directory
-      mermaid_js_src: '/assets/mermaid.min.js',
-      html_tag: 'div',
-      extra_classes: 'graph'
+
+  function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav"].indexOf(tag) === -1);
+  }
+  eleventyConfig.setDataDeepMerge(true);
+
+  function filterTagList(tags) {
+    return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList)
+
+  eleventyConfig.addCollection("tagList", collection => {
+    const tagsObject = {}
+    collection.getAll().forEach(item => {
+      if (!item.data.tags) return;
+      item.data.tags
+        .filter(tag => !['post', 'all'].includes(tag))
+        .forEach(tag => {
+          if(typeof tagsObject[tag] === 'undefined') {
+            tagsObject[tag] = 1
+          } else {
+            tagsObject[tag] += 1
+          }
+        });
     });
 
-    // eleventyConfig.addPlugin(eleventyPluginSyntaxHighlighter);
-    
-    return {
-      dir: {
-        input: "src",
-        output: "_site"
-      }
+    const tagList = []
+    Object.keys(tagsObject).forEach(tag => {
+      tagList.push({ tagName: tag, tagCount: tagsObject[tag] })
+    })
+    return tagList.sort((a, b) => b.tagCount - a.tagCount)
+
+  });
+
+
+  // Add a filter using the Config API
+  eleventyConfig.addWatchTarget("./src/scss/");
+  eleventyConfig.setBrowserSyncConfig({
+    reloadDelay: 400
+  });
+
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat("dd LLL yyyy");
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('yyyy-LL-dd');
+  });
+  return {
+    dir: {
+      input: "src",
+      output: "dev"
     }
   };
+
+};
