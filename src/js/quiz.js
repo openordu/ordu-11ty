1cdbc04139efc486df30bb3567edee8fa8ebf958
Chris Godwin <patchshorts@gmail.com>
2023-05-12 18:30:50 -0500

    quiz fully works

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,40 +1,41 @@
-let currentQuestion = 0;
-let score = 0;
-const quizContainer = document.getElementById("quiz");
-const nextBtn = document.getElementById("nextBtn");
-const result = document.getElementById("result");
-const questions = document.getElementsByClassName("question");
-
-function showQuestion() {
-    for (let i = 0; i < questions.length; i++) {
+document.addEventListener("DOMContentLoaded", function() {
+    let currentQuestion = 0;
+    let score = 0;
+    const parentContainer = document.getElementById("parentContainer");
+    const quizContainer = parentContainer.querySelector("#quiz");
+    const nextBtn = parentContainer.querySelector("#nextBtn");
+    const result = parentContainer.querySelector("#score");
+    const questions = parentContainer.getElementsByClassName("question");
+  
+    function showQuestion() {
+      for (let i = 0; i < questions.length; i++) {
         questions[i].style.display = (i === currentQuestion) ? "block" : "none";
+      }
     }
-}
-
-quizContainer.addEventListener('click', (event) => {
-    const target = event.target;
-    if (target.tagName === 'BUTTON') {
+  
+    quizContainer.addEventListener('click', (event) => {
+      const target = event.target;
+      if (target.tagName === 'BUTTON') {
+        target.classList.remove('list-group-item-danger');
         const selectedIndex = Array.from(target.parentNode.children).indexOf(target);
         const correctAnswer = parseInt(questions[currentQuestion].getAttribute('data-answer'));
         if (selectedIndex === correctAnswer) {
-            target.classList.add('list-group-item-success');
-            score++;
-            setTimeout(() => {
-                currentQuestion++;
-                if (currentQuestion < questions.length) {
-                    showQuestion();
-                } else {
-                    quizContainer.innerHTML = '';
-                    result.innerHTML = `<h2>Your score: ${score}</h2>`;
-                }
-            }, 3000);
+          score++;
+          result.innerHTML = `${score}`;
+          target.classList.add('list-group-item-success');
+          setTimeout(() => {
+            currentQuestion++;
+            if (currentQuestion < questions.length) {
+              showQuestion();
+            } else {
+              quizContainer.innerHTML = '';
+            }
+          }, 3000);
         } else {
-            target.classList.add('list-group-item-danger');
-            setTimeout(() => {
-                target.classList.remove('list-group-item-danger');
-            }, 3000);
+          target.classList.add('list-group-item-danger');
         }
-    }
-});
-
-showQuestion();
+      }
+    });
+  
+    showQuestion();
+  });  
